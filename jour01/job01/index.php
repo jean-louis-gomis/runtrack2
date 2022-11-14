485131c2a08b587e01fed4846ba10652fadb48c1
jean-louis-gomis <114387379+jean-louis-gomis@users.noreply.github.com>
2022-11-14 18:19:31 +0100

    modif 14-11-2022-18h20

---
+++ jour01/job01/index.php
@@ -0,0 +1,36 @@
+<?php 
+$str = "LaPlateforme" ;
+$str2 = "Vive " ;
+$str3 = "!" ;
+$strtotal = $str2.$str.$str3;
+    echo $str; 
+
+?>
+
+
+<?php 
+$str = "LaPlateforme" ;
+$str2 = "Vive " ;
+$str3 = "!" ;
+$strtotal = $str2.$str.$str3;
+
+    echo $strtotal;
+?>
+
+
+<?php 
+$val = 6 ;
+
+
+    echo $val + 4;
+
+?>
+
+<?php 
+// $myBool = true ;
+$myBool = false ;
+
+
+    echo $myBool;
+
+?>
