29e9ffad6ae8c0bac8aeed28e5f6f534f6f57ffd
jean-louis-gomis <114387379+jean-louis-gomis@users.noreply.github.com>
2022-11-16 04:05:37 +0100

    job03

--- jour02/job01/index.php
+++ jour02/job01/index.php
@@ -5,7 +5,7 @@ $bold = 42;
 
 while ($dep < $fin) {
     if ($dep === 42) {
-        echo "<b>$bold</b>";
+        echo "<b><b/>"."<u></u>";
     }
     echo $dep. '<br>';
     $dep = $dep + 1;
--- jour02/job02/index.php
+++ jour02/job02/index.php
@@ -1,5 +1,32 @@
 <?php
+$dep = 0;
+$fin = 1337;
+$ex26 = 26;
 
+
+while ($dep < 1337) {
+    if ($dep == 26) {
+        $dep++;
+        continue;
+    }
+    if ($dep == 37) {
+        $dep++;
+        continue;
+    }
+    if ($dep == 88) {
+        $dep++;
+        continue;
+    }
+    if ($dep == 1111) {
+        $dep++;
+        continue;
+    }
+    echo $dep++. '<br>';
+
+    
+
+
+}
 
 
 
