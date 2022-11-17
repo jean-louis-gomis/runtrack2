3a5701d84a231bef7aa8b6b4734ccfab48a4b9a8
jean-louis-gomis <114387379+jean-louis-gomis@users.noreply.github.com>
2022-11-17 14:59:37 +0100

    job02

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -1,5 +1,14 @@
 <?php
 $str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie."
+$compt = 
+    for ($i=0;  < $i++) 
+        # code...
+    }
+    for ($i=0; $i <= 78; $i++) {
+        echo $str[$i];
+
+        
+    }
 
 
 
